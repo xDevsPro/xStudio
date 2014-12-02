362486e7bb9339c11d9b43fde95c4658400262e8
Miguel Abreu Ortega <mabreuortega@gmail.com>
2014-12-02 00:26:47 -0500

    Add UnitTest project

---
+++ Solution/Tests/xStudio.UnitTests/SanityCheck.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace xStudio.UnitTests
+{
+    public class SanityCheck
+    {
+        [Fact]
+        public void Pass()
+        {
+            Assert.True(true);
+        }
+    }
+}
