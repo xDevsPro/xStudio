839ff7e76d47cdff18db5e1125938e71394a3633
Miguel Abreu Ortega <mabreuortega@gmail.com>
2014-12-03 00:35:40 -0500

    GREEN - Implemente TranslatedString class

--- Solution/Common/xStudio.Common/Localization/TranslatedString.cs
+++ Solution/Common/xStudio.Common/Localization/TranslatedString.cs
@@ -5,20 +5,44 @@ namespace xStudio.Common.Localization
 {
     public class TranslatedString : IHtmlString
     {
-        public TranslatedString(string pattern, params object[] parameters)
+        private readonly string _localized;
+
+        public TranslatedString(string text, params object[] args)
         {
-            throw new NotImplementedException();
+            if (args != null && args.Length > 0)
+                _localized = string.Format(text, args);
+            else
+                _localized = text;
         }
 
+        public override string ToString()
+        {
+            return _localized;
+        }
         public string ToHtmlString()
         {
-            throw new NotImplementedException();
+            return _localized;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 0;
+            if (_localized != null)
+                hashCode ^= _localized.GetHashCode();
+            return hashCode;
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null || obj.GetType() != GetType())
+                return false;
+
+            var that = (TranslatedString) obj;
+            return string.Equals(_localized, that._localized);
         }
 
         public static implicit operator string(TranslatedString text)
         {
-            throw new NotImplementedException();
-            //return text == null ? null : text.ToString();
+            return text == null ? null : text.ToString();
         }
     }
 }
