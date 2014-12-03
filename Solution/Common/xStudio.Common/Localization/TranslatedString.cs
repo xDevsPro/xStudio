3402e07c573f9c813395833c18578d94a5d0fff9
Miguel Abreu Ortega <mabreuortega@gmail.com>
2014-12-03 00:32:43 -0500

    RED - Add Tranlated String contract

---
+++ Solution/Common/xStudio.Common/Localization/TranslatedString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web;
+
+namespace xStudio.Common.Localization
+{
+    public class TranslatedString : IHtmlString
+    {
+        public TranslatedString(string pattern, params object[] parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ToHtmlString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static implicit operator string(TranslatedString text)
+        {
+            throw new NotImplementedException();
+            //return text == null ? null : text.ToString();
+        }
+    }
+}
---
+++ Solution/Tests/xStudio.UnitTests/Common/Localization/TranslatedStringTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Web;
+using xStudio.Common.Localization;
+using Xunit;
+using Xunit.Should;
+
+namespace xStudio.UnitTests.Common.Localization
+{
+    public class TranslatedStringTests
+    {
+        [Fact]
+        public void CanLocalizeWithPatternAndArgs()
+        {
+            var str = new TranslatedString("a{0}", 1);
+            "a1".ShouldBe(str.ToString());
+        }
+
+        [Fact]
+        public void CanConvertToHtmlString()
+        {
+            var str = new TranslatedString("blah");
+            var html = str as IHtmlString;
+
+            html.ShouldNotBeNull();
+            "blah".ShouldBe(html.ToHtmlString());
+        }
+
+        [Fact]
+        public void EqualsComparesContent()
+        {
+            var a = new TranslatedString("a");
+            var b = new TranslatedString("a");
+            a.ShouldBe(b);
+        }
+
+        [Fact]
+        public void EqualsComparesOnlyWithLocalizedString()
+        {
+            var a = new TranslatedString("a");
+            a.ShouldNotBe(null);
+            a.ShouldNotBe(new object());
+        }
+
+        [Fact]
+        public void SameContentGetSameHashcode()
+        {
+            var a = new TranslatedString("a");
+            var b = new TranslatedString("a");
+            a.GetHashCode().ShouldBe(b.GetHashCode());
+        }
+
+        [Fact]
+        public void ItsAnImplicitString()
+        {
+            string str = new TranslatedString("blah");
+            "blah".ShouldBe(str);
+        }
+    }
+}
